aa20e53696c1efdbff6d16366d6bb313e72d874a
Rohan Singh <rohansinghwow@gmail.com>
2021-11-22 21:28:36 +0530

    Adding Post logic

--- index.js
+++ index.js
@@ -1,58 +1,75 @@
-/** Array to store the posts **/
- 
- let postsArray = []
 
- const URL = 'https://apis.scrimba.com/jsonplaceholder/posts'
+let storeArray = [ ]
 
- const postTitleEl = document.getElementById('post-title')
- const postBodyEl = document.getElementById('post-body')
- const submitbtn = document.getElementByClassName('post-btn')
+let blogEl = document.getElementById('blog-list')
+let sendForm = document.getElementById('new-post')
+renderBlogs()
 
- let blog = document.getElementById('blog-list')
 
- const options = {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }
+function renderBlogs(){
 
-submitbtn.addEventListener('submit', function(e){
+
+	fetch('https://apis.scrimba.com/jsonplaceholder/posts')
+	.then(response => response.json())
+	.then(data => {
+	storeArray = data.slice(0,5)
+		console.log(storeArray)
+	let html = ``
+
+			for(let post of storeArray){
+
+				html+= 
+
+				`
+
+				<div class="blog-container">
+		                <h1>${post.title}</h1>
+		                <hr>
+		                 <p>${post.body}</p>
+		        </div>
+
+			`
+			blogEl.innerHTML = html
+		}
+	
+	})
+	
+}
+
+sendForm.addEventListener('submit' , function(e){
 	e.preventDefault()
-	postTitle = postTitleEl.value
-	postBody = postBodyEl.value 
+	getNewBlog()
 })
 
- function getPosts(){
 
- 	fetch(URL , options)
- 	.then(response => response.json())
- 	.then(data => {
- 		let postsArray = data.slice(0,5)
+function getNewBlog(){
+	
+	let postTitle = document.getElementById('post-title').value
+	let postBody = document.getElementById('post-body').value
 
- 	})
+	const data = {
+		title : postTitle,
+		body: postBody
+	}
+	const options = {
+		method : "POST",
+		body : JSON.stringify(data),
+		headers: {
+            "Content-Type": "application/json"
+        }
 
- }
+	}
 
- function addNewPost(){
- 	postsArray.unshift()
- }
+	fetch('https://apis.scrimba.com/jsonplaceholder/posts' , options)
+	.then(response => response.json())
+	.then(data=>{
+		storeArray.unshift(data)
+		renderBlogs()
+	})
+}
 
- function renderPosts(){
 
- 	blog.innerHTML = ''
+	
 
- 	for(let post of postsArray){
- 		blog += `
+	
 
- 		<div class="blog-container">
-                <h1>${post.title}</h1>
-                <hr>
-                 <p>${post.body}</p>
-            </div>
-
- 		`
- 	}
-
- }
